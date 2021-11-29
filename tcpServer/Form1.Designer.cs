b019d14657f90d8c4ebc7b85ff0b5ea624bcafed
Always1010 <2321216017@qq.com>
2021-11-29 14:44:18 +0800

    添加了服务端代码

---
+++ tcpServer/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.ChatBox = new System.Windows.Forms.TextBox();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.Clean = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(713, 91);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // ChatBox
+            // 
+            this.ChatBox.Location = new System.Drawing.Point(3, 88);
+            this.ChatBox.Multiline = true;
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.Size = new System.Drawing.Size(489, 331);
+            this.ChatBox.TabIndex = 2;
+            // 
+            // Port
+            // 
+            this.Port.Location = new System.Drawing.Point(572, 92);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(120, 25);
+            this.Port.TabIndex = 4;
+            // 
+            // Clean
+            // 
+            this.Clean.Location = new System.Drawing.Point(668, 412);
+            this.Clean.Name = "Clean";
+            this.Clean.Size = new System.Drawing.Size(75, 23);
+            this.Clean.TabIndex = 5;
+            this.Clean.Text = "清空";
+            this.Clean.UseVisualStyleBackColor = true;
+            this.Clean.Click += new System.EventHandler(this.Clean_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(511, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "port";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(514, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 274);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Clean);
+            this.Controls.Add(this.Port);
+            this.Controls.Add(this.ChatBox);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox ChatBox;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.Button Clean;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ tcpServer/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace Server
+{
+
+    public partial class Form1 : Form
+    {
+        IPEndPoint listenPort = null;
+
+        Thread threadWatch = null;
+        Socket socket = null;
+        Dictionary<string, Socket> sockets = new Dictionary<string, Socket>();
+        Dictionary<string, Thread> dictThread = new Dictionary<string, Thread>();
+        Dictionary<string, string> users = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Port.Text = "10086";
+            //System.Diagnostics.Debug.WriteLine("jjjjj................." + Thread.CurrentThread.ManagedThreadId.ToString());
+        }
+
+        private void Clean_Click(object sender, EventArgs e)
+        {
+            ChatBox.Text = "";
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            int port = Convert.ToInt32(Port.Text);
+            listenPort = new IPEndPoint(IPAddress.Any, port);
+            socket.Bind(listenPort);
+            socket.Listen(10000);
+            threadWatch = new Thread(Listen);
+            threadWatch.IsBackground = true;
+            threadWatch.Start();
+            Start.Enabled = false;
+        }
+        public void Listen()
+        {
+            //System.Diagnostics.Debug.WriteLine("jjjjj.！....." + Thread.CurrentThread.ManagedThreadId);
+            while (true)
+            {
+                Socket sokConnection = socket.Accept();
+                //this.listBox1.Invoke(new Action(() =>
+                //{
+                //    listBox1.Items.Add(sokConnection.RemoteEndPoint.ToString());
+                //}));
+                this.listBox1.Invoke(new Action(() =>
+                {
+                    sokConnection.RemoteEndPoint.ToString();
+                }));
+                sockets.Add(sokConnection.RemoteEndPoint.ToString(), sokConnection);
+
+               
+                //System.Diagnostics.Debug.WriteLine("jjjjj.客户端连接成功！....." + Thread.CurrentThread.Name);
+                
+                //foreach (Socket s in sockets.Values)
+                //{
+                //    byte[] massage = System.Text.Encoding.UTF8.GetBytes("$b"+'|'+sokConnection.RemoteEndPoint.ToString()+'|');
+                //    s.Send(massage);
+                //}
+
+
+                Thread thr = new Thread(msgRec);
+                thr.IsBackground = true;
+                thr.Start(sokConnection);
+                dictThread.Add(sokConnection.RemoteEndPoint.ToString(), thr);
+            }
+        }
+
+        public void msgRec(object sokConnectionparn)
+        {
+            Socket sokClient = sokConnectionparn as Socket;
+            while (true)
+            {
+                byte[] arrMsgRec = new byte[1024];
+                int length = -1;
+                try
+                {
+                    length = sokClient.Receive(arrMsgRec);
+                }
+                catch (SocketException se)
+                {
+                    sockets.Remove(sokClient.RemoteEndPoint.ToString());
+                    dictThread.Remove(sokClient.RemoteEndPoint.ToString());
+                    deleteUser(sokClient.RemoteEndPoint.ToString());
+                    updateUsers();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    sockets.Remove(sokClient.RemoteEndPoint.ToString());
+                    dictThread.Remove(sokClient.RemoteEndPoint.ToString());
+                    deleteUser(sokClient.RemoteEndPoint.ToString());
+                    updateUsers();
+                    break;
+                }
+
+                if (arrMsgRec != null)
+                {
+                    string strMsg = System.Text.Encoding.UTF8.GetString(arrMsgRec, 0, length);
+                    string[] spl = strMsg.Split('|');
+
+                    //待优化
+                    //群发消息
+                    if (spl[0] == "$a")
+                    {
+                        this.ChatBox.Invoke(new Action(() =>
+                        {
+                            this.ChatBox.AppendText(spl[1]);
+                        }));
+
+                        if (length != -1)
+                            foreach (Socket s in sockets.Values)
+                            {
+                                s.Send(arrMsgRec);
+                            }
+                    }
+
+
+                    //接入用户
+                    if (spl[0] == "$b")
+                    {
+                        users.Add(spl[1], sokClient.RemoteEndPoint.ToString());
+                        this.listBox1.Invoke(new Action(() =>
+                        {
+                            listBox1.Items.Add(spl[1]);
+                        }));
+                        updateUsers();
+                    }
+
+
+                    //下棋
+                    if (spl[0] == "$c" )
+                    {
+                        //foreach(var e in sockets)
+                        //{
+                        //    System.Diagnostics.Debug.WriteLine(e.Key);
+                        //}
+                        //System.Diagnostics.Debug.WriteLine(spl[1]);
+                        sockets[users[spl[1]]].Send(arrMsgRec);
+
+                    }
+
+
+                    //发起五子棋连接
+                    if (spl[0] == "$s")
+                    {
+                        //string data = "$s" + '|'  +  + '|';
+                        //arrMsgRec = System.Text.Encoding.UTF8.GetBytes(data);
+                        sockets[users[spl[1]]].Send(arrMsgRec);
+                    }
+
+                    if (spl[0] == "$g")
+                    {
+                        sockets[users[spl[1]]].Send(arrMsgRec);
+                    }
+
+                    //双方准备
+                    if (spl[0] == "$z")
+                    {
+                        sockets[users[spl[1]]].Send(arrMsgRec);
+                    }
+
+                    if (spl[0] == "$r")
+                    {
+                        sockets[users[spl[1]]].Send(arrMsgRec);
+                    }
+
+
+                    //文件传输
+                    if (spl[0] == "$f")
+                    {
+                        sockets[users[spl[1]]].Send(arrMsgRec);
+                    }
+
+
+                    //this.ChatBox.Invoke(new Action(() =>
+                    //{
+                    //    this.ChatBox.AppendText(strMsg);
+                    //}));
+
+                    //if (length != -1)
+                    //    foreach (Socket s in sockets.Values)
+                    //    {
+                    //        s.Send(arrMsgRec);
+                    //    }
+                }
+            }
+
+        }
+
+        
+        public void deleteUser(string str)
+        {
+            
+            string strr = "";
+            foreach (var item in users)
+            {
+                if (item.Value == str)
+                {
+                    strr = item.Key;
+                    break;
+                }
+                
+            }
+            this.listBox1.Invoke(new Action(() =>
+            {
+                listBox1.Items.Remove(strr);
+            }));
+            users.Remove(strr);
+        }
+
+        public string getUsers()
+        {
+            string strr = "$b"+'|';
+            foreach (var item in users)
+            {
+                strr += item.Key + '|';
+            }
+            return strr;
+        }
+
+
+        //更新在线用户名单
+        public void updateUsers()
+        {
+            string usersName = getUsers();
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(usersName);
+            foreach (Socket s in sockets.Values)
+            {
+                s.Send(data);
+            }
+        }
+    }
+}
